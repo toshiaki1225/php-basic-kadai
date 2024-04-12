a8c99954b39b1f0720a7b9cd202e3362ccca8ffb
toshiaki1225 <125113596+toshiaki1225@users.noreply.github.com>
2024-04-13 07:13:27 +0900

    kadai_007をアップロード
    kadai_007をアップロードしました。

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP課題</title>
+</head>
+<body>
+  <p>
+    <?php
+      $Array = ['name'=>'onion', 'price'=>'200', 'weight'=>'160'];
+      print_r($Array);
+    ?>
+  </p>
+</body>
+</html>
